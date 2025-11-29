726751b0c9bfa7c513ddc48b5e6eed9c0795635e
mirac <mirac.i.sancak@gmail.com>
2025-11-29 22:52:01 +0100

    Erste Veränderungen: Navbar und Design Vorstellung fertig, jetzt zum Header

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,14 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script setup lang="ts">
+import Header from "./components/Header.vue";
+import Nav from "./components/Navbar.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <Nav />
+  <Header />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
+/* hier kannst du später Styles für die Seite machen */
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
